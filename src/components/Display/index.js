5a4f7e51f819d80dddb25e3ee703c5dc374be002
drivenfw <ivan.petrochenko@gmail.com>
2018-09-08 08:34:29 +0300

    Display added

---
+++ src/components/Display/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledDisplay = styled.div`
+  height: 200px;
+  padding: 10px;
+  border: 1px solid grey;
+  border-radius: 10px;
+  box-shadow: inset 0 0 3px 2px lightgrey;
+  overflow-y: scroll;
+`
+
+const Display = () =>
+  <StyledDisplay />
+
+export default Display
+
--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import Display from '../components/Display'
+
 
 const StyledAppLayout = styled.div`
   height: 100vh;
@@ -9,19 +11,23 @@ const StyledAppLayout = styled.div`
 const NavBar = styled.nav`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+  padding: 20px;
 `
 
 const NavButton = styled.button`
   width: 120px;
   font-size: 1em;
   padding: 0.5em;
-  margin: 1em 0.5em;
   outline: none;
   border: 1px solid grey;
   border-radius: 100px / 70px;
   box-shadow: 0 0 5px 3px lightgrey;
   background: white;
+`
+
+const Content = styled.main`
+  padding: 0 20px 20px 20px;
 `
 
 class AppLayout extends Component {
@@ -32,6 +38,9 @@ class AppLayout extends Component {
           <NavButton>Game</NavButton>
           <NavButton>Settings</NavButton>
         </NavBar>
+        <Content>
+          <Display />
+        </Content>
       </StyledAppLayout>
     )
   }
