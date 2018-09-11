f7778671502b07d025fafdf1aee8e7b51d38e8b7
drivenfw <ivan.petrochenko@gmail.com>
2018-09-11 16:41:14 +0300

    Input styling improved

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,20 +4,32 @@ import styled from 'styled-components'
 
 const StyledInput = styled.div`
   width: 40px;
+  margin: 5px;
 `
 
 const UpButton = styled.div`
   font-size: 2em;
   text-align: center;
-  transform: scaleX(1.5) scaleY(1.5);
+  transform: scaleX(1.6) scaleY(1.4);
   color: grey;
+  cursor: pointer;
+  user-select: none;
+  text-shadow:
+    0 2px 7px rgba(255,255,255,0.8),
+    0 5px 5px rgba(0,0,0,0.4);
+  transition: all 0.1s ease;
+
+  &:active {
+    transform: scaleX(1.6) scaleY(1.4) translateY(1px);
+    text-shadow: 0 2px 3px rgba(0,0,0,0.3);
+  }
 `
 
 const ScrollingInput = styled.div`
   box-sizing: border-box;
   width: 40px;
   height: 70px;
-  margin: 5px 0;
+  margin: 10px 0 15px 0;
   border: 1px solid grey;
   border-radius: 2px;
   box-shadow: 0 0 5px 3px lightgrey;
@@ -26,8 +38,19 @@ const ScrollingInput = styled.div`
 const DownButton = styled.div`
   font-size: 2em;
   text-align: center;
-  transform: scaleX(1.5) scaleY(1.5);
+  transform: scaleX(1.6) scaleY(1.4);
   color: grey;
+  cursor: pointer;
+  user-select: none;
+  text-shadow:
+    0 -2px 7px rgba(255,255,255,0.8),
+    0 -5px 5px rgba(0,0,0,0.3);
+  transition: all 0.1s ease;
+
+  &:active {
+    transform: scaleX(1.6) scaleY(1.4) translateY(-1px);
+    text-shadow: 0 -2px 3px rgba(0,0,0,0.3);
+  }
 `
 
 const Input = () =>
