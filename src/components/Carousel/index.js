50a364e639cbe12e8f4021e50ad4239d822fcf3a
drivenfw <ivan.petrochenko@gmail.com>
2018-09-16 22:00:56 +0500

    The Carousel component implemented

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 
 const StyledCarousel = styled.div`
@@ -9,15 +10,61 @@ const StyledCarousel = styled.div`
   border: 1px solid grey;
   border-radius: 2px;
   box-shadow: 0 0 5px 3px lightgrey;
+  position: relative;
+  overflow: hidden;
+
+  & .turn-down-enter {
+    top: -100%;
+    transition: top 500ms cubic-bezier(0, 0.7, 0.25, 1.19);
+  }
+
+  & .turn-down-enter-active {
+    top: 0;
+  }
+
+  & .turn-down-exit {
+    top: 0;
+    transition: top 500ms cubic-bezier(0, 0.7, 0.25, 1.19);
+  }
+
+  & .turn-down-exit-active {
+    top: 100%;
+  }
+
+  & .turn-up-enter {
+    top: 100%;
+    transition: top 500ms cubic-bezier(0, 0.7, 0.25, 1.19);
+  }
+
+  & .turn-up-enter-active {
+    top: 0;
+  }
+
+  & .turn-up-exit {
+    top: 0;
+    transition: top 500ms cubic-bezier(0, 0.7, 0.25, 1.19);
+  }
+
+  & .turn-up-exit-active {
+    top: -100%;
+  }
 `
 
-class Carousel extends Component {
-  render() {
-    return (
-      <StyledCarousel />
-    )
-  }
-}
+const Carousel = ({ children, direction }) => 
+  <StyledCarousel>
+    <TransitionGroup>
+      {React.Children.map(children, child => (
+        <CSSTransition
+          timeout={500}
+          classNames={`turn-${direction}`}
+          mountOnEnter
+          unmountOnExit
+        >
+          {child}
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  </StyledCarousel>
 
 export default Carousel
 
