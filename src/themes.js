debf6ce2b3d6064bfe64f35c0f3c845fca06433a
drivenfw <ivan.petrochenko@gmail.com>
2018-10-02 16:29:21 +0300

    Red-yellow theme added

--- src/themes.js
+++ src/themes.js
@@ -4,6 +4,12 @@ export default {
     primaryColor1: '#e497b0',
     primaryColor2: '#f1cbd7',
     secondaryColor: '#fff8ee'
+  },
+  redYellow: {
+    primaryColor: '#F06653',
+    primaryColor1: '#F39C90',
+    primaryColor2: '#F8C8C2',
+    secondaryColor: '#FFF8C8'
   }
 }
 
