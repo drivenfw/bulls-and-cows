fb11509c7c4ac14b9611910b8d10282bb441bc80
drivenfw <ivan.petrochenko@gmail.com>
2018-10-09 13:35:36 +0300

    Display reducer resets screen on play and stop actions

--- src/reducers/display.js
+++ src/reducers/display.js
@@ -1,24 +1,30 @@
 import { createReducer } from 'redux-act'
 
+import { play, stop } from '../actions/controls'
+
 
 const initialState = {
   content: [
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum',
-   'Lorem ipsum'
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum',
+    'Lorem ipsum'
   ],
   scroll: 0
 }
 
 const display = createReducer({
-
+  [play]: () => ({
+    content: [],
+    scroll: 0
+  }),
+  [stop]: () => initialState
 }, initialState)
 
 export default display
