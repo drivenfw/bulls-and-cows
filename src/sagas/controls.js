ae9a830a7c163fbc189f446c4dd5d7e12c328939
drivenfw <ivan.petrochenko@gmail.com>
2018-10-09 18:23:24 +0300

    Play, stop and submit sagas implemented

--- src/sagas/controls.js
+++ src/sagas/controls.js
@@ -1,13 +1,51 @@
-import { takeEvery } from 'redux-saga/effects'
+import { put, select, takeEvery } from 'redux-saga/effects'
 
+import Game from '../app/Game'
 import { tickHandler } from './clock'
+import { addContent, clear } from '../actions/display'
 
 
-function *playHandler(playAction) {
+let game = null
+
+function *playHandler() {
+  if (!game) {
+    const { settings: { options } } = yield select()
+
+    // TODO: use reselect
+    game = new Game(Array.from({ length: options }, (_, i) => i + 1))
+    game.start()
+
+    yield put(clear())
+  }
+
   yield *tickHandler()
+}
+
+function stopHandler() {
+  game = null
+}
+
+const calcScroll = contentLength => 
+  contentLength > 6 ? contentLength - 6 : 0
+
+
+function *submitHandler(action) {
+  const result = game.guess(action.payload)
+  const { display: { content } } = yield select()
+
+  yield put(addContent({
+    content: `${action.payload}: ${result}`,
+    scroll: calcScroll(content.length + 1)
+  }))
+
+  if (result === '++++') {
+    // TODO: Congratulate on winning
+  }
 }
 
 export function *watchControlsActions() {
   yield takeEvery('CONTROLS_PLAY', playHandler)
+  yield takeEvery('CONTROLS_STOP', stopHandler)
+  yield takeEvery('CONTROLS_SUBMIT', submitHandler)
 }
 
