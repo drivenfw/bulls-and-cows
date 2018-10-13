adf98e85df189048d96850839f3098e9ce9070e2
drivenfw <ivan.petrochenko@gmail.com>
2018-10-13 20:46:10 +0300

    hexToRgba helper used the themes object

--- src/themes.js
+++ src/themes.js
@@ -1,15 +1,20 @@
+import { hexToRgba } from './helpers/color'
+
+
+const primaryColor1 = color => hexToRgba(color, 0.6)
+
+const primaryColor2 = color => hexToRgba(color, 0.3)
+
 export default {
   main: {
     primaryColor: '#db7093',
-    primaryColor1: '#e497b0',
-    primaryColor2: '#f1cbd7',
-    secondaryColor: '#fff8ee'
+    primaryColor1: primaryColor1('#db7093'),
+    primaryColor2: primaryColor2('#db7093')
   },
   redYellow: {
     primaryColor: '#F06653',
     primaryColor1: '#F39C90',
-    primaryColor2: '#F8C8C2',
-    secondaryColor: '#FFF8C8'
+    primaryColor2: '#F8C8C2'
   }
 }
 
