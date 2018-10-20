cb46ee91970cacdd92caf20caa413f8b04f840e2
drivenfw <ivan.petrochenko@gmail.com>
2018-10-20 17:59:12 +0300

    Select component added

---
+++ src/components/Select/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+import Carousel, { Value } from 'components/Carousel'
+
+
+export const Option = styled.div`
+  width: 100%;
+  text-align: center;
+`
+
+const StyledSelect = styled.div`
+  display: flex;
+  align-items: center; 
+  padding: 3px;
+  overflow: hidden;
+`
+
+const StyledCarousel = styled(Carousel)`
+  width: 200px;
+  height: 35px;
+  font-size: 1.6em;
+
+  @media (min-width: 850px) {
+    width: 300px;
+    height: 40px;
+    font-size: 1.8em;
+  }
+`
+
+const Button = styled.div`
+  font-size: 1.8em;
+  text-align: center;
+  cursor: pointer;
+  user-select: none;
+  transition: all 0.1s ease;
+
+  ${({ theme }) => css`
+    color: ${theme.primaryColor};
+    text-shadow:
+      0 2px 7px ${theme.primaryColor1},
+      0 5px 5px ${theme.primaryColor2};
+  `}
+`
+
+const LeftButton = styled(Button)`
+  margin-right: 5px;
+  transform: rotate(-90deg) scaleX(2.2) scaleY(0.9);
+
+  &:active {
+    transform: rotate(-90deg) scaleX(2.2) scaleY(0.8) translateY(1px);
+    text-shadow: 0 2px 3px rgba(0,0,0,0.3);
+  }
+`
+
+const RightButton = styled(Button)`
+  margin-left: 5px;
+  transform: rotate(90deg) scaleX(2.2) scaleY(0.9);
+
+  &:active {
+    transform: rotate(90deg) scaleX(2.2) scaleY(0.9) translateY(1px);
+    text-shadow: 0 2px 3px rgba(0,0,0,0.3);
+  }
+`
+
+class Select extends Component {
+  direction = 'right'
+
+  left = () => {
+    const { children, value, onChange } = this.props
+
+    const childArray = React.Children.toArray(children)
+    const index = childArray.findIndex(child => child.props.value === value)
+    const nextIndex = index > 0 ? index - 1 : childArray.length - 1
+
+    this.direction = 'left'
+    onChange(childArray[nextIndex].props.value)
+  } 
+
+  right = () => {
+    const { children, value, onChange } = this.props
+
+    const childArray = React.Children.toArray(children)
+    const index = childArray.findIndex(child => child.props.value === value)
+    const nextIndex = index < childArray.length - 1 ? index + 1 : 0
+
+    this.direction = 'right'
+    onChange(childArray[nextIndex].props.value)
+  }
+
+  render() {
+    const { children, value } = this.props
+    const selected = React.Children.toArray(children)
+      .find(child => child.props.value === value)
+
+    return (
+      <StyledSelect>
+        <LeftButton onClick={this.left}>⌃</LeftButton>
+        <StyledCarousel direction={this.direction}>
+          <Value key={value}>{selected}</Value>
+        </StyledCarousel>
+        <RightButton onClick={this.right}>⌃</RightButton>
+      </StyledSelect>
+    )
+  }
+}
+
+export default Select
+
