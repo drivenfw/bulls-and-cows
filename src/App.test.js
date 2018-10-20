6ec3299adcbda4d055055a80ca9b3ac96a81eb4a
drivenfw <ivan.petrochenko@gmail.com>
2018-10-20 12:14:56 +0300

    App component tests fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App'
+
+import reducer from 'reducers'
+
+
+const store = createStore(reducer)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
+
