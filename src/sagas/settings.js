93f3cf0830d99132722bd4ca5051c12683cd7c41
drivenfw <ivan.petrochenko@gmail.com>
2018-10-24 16:31:34 +0300

    watchSettingsActions saga refactored

--- src/sagas/settings.js
+++ src/sagas/settings.js
@@ -1,40 +1,44 @@
-import { select, takeEvery } from 'redux-saga/effects'
+import { select, take } from 'redux-saga/effects'
 
 import { SettingsStorage } from 'app/Storage'
+import { 
+  switchDifficulty,
+  switchLocale,
+  switchTheme,
+  toggleRememberSettings
+} from 'actions/settings'
 
 
 const settingsStorage = new SettingsStorage()
 
-function *switchDifficultyHandler(action) {
-  const { settings: { rememberSettings } } = yield select()
-
-  rememberSettings && settingsStorage.setSetting('difficulty', action.payload)
-}
-
-function *switchLocaleHandler(action) {
-  const { settings: { rememberSettings } } = yield select()
- 
-  rememberSettings && settingsStorage.setSetting('locale', action.payload)
-}
-
-function *switchThemeHandler(action) {
-  const { settings: { rememberSettings } } = yield select()
- 
-  rememberSettings && settingsStorage.setSetting('theme', action.payload)
-}
-
-function *toggleRememberSettingsHandler(action) {
-  const { settings } = yield select()
-
-  action.payload 
-    ? settingsStorage.setAllSettings(settings)
-    : settingsStorage.resetSettings()
-}
+const matchSettingsActions = action => 
+  /SETTINGS/.test(action.type)
 
 export function *watchSettingsActions() {
-  yield takeEvery('SETTINGS_SWITCH_DIFFICULTY', switchDifficultyHandler)
-  yield takeEvery('SETTINGS_SWITCH_LOCALE', switchLocaleHandler)
-  yield takeEvery('SETTINGS_SWITCH_THEME', switchThemeHandler)
-  yield takeEvery('SETTINGS_TOGGLE_REMEMBER_SETTINGS', toggleRememberSettingsHandler)
+  const handlers = {
+    [switchDifficulty]: (save, action) => {
+      save && settingsStorage.setSetting('difficulty', action.payload)
+    },
+    [switchLocale]: (save, action) => {
+      save && settingsStorage.setSetting('locale', action.payload)
+    },
+    [switchTheme]: (save, action) => {
+      save && settingsStorage.setSetting('theme', action.payload)
+    },
+    [toggleRememberSettings]: function* (save, action) {
+      const { settings } = yield select()
+
+      action.payload 
+        ? settingsStorage.setAllSettings(settings)
+        : settingsStorage.resetSettings()
+    }
+  }
+
+  while (true) {
+    const action = yield take(matchSettingsActions)
+    const { settings: { rememberSettings } } = yield select()
+
+    yield *handlers[action.type](rememberSettings, action)
+  }
 }
 
