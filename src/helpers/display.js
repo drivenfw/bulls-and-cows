207c25abaa91699e6b07d2c43a1092bd24d49b36
drivenfw <ivan.petrochenko@gmail.com>
2018-10-29 18:22:33 +0300

    nbspPadding helper removed

--- src/helpers/display.js
+++ src/helpers/display.js
@@ -1,9 +1,6 @@
-export const calcScroll = contentLength => {
+export const calcScroll = contentLength => { // TODO: add tests
   const lines = matchMedia('(min-width: 375px)').matches ? 6 : 5
 
   return contentLength > lines ? contentLength - lines : 0
 }
-
-export const nbspPadding = length =>
-  [...Array(length)].map(() => '\u00a0').join('')
 
