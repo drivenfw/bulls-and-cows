03654da03e02995cf78e2d9fd8faadb7ab684199
drivenfw <ivan.petrochenko@gmail.com>
2018-10-30 22:49:28 +0300

    Refactoring

--- src/helpers/display.js
+++ src/helpers/display.js
@@ -1,4 +1,4 @@
-export const calcScroll = contentLength => { // TODO: add tests
+export const calcScroll = contentLength => {
   const lines = matchMedia('(min-width: 375px)').matches ? 6 : 5
 
   return contentLength > lines ? contentLength - lines : 0
