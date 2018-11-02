e48d01eaf0b4401773041209647b1f12835b8c26
drivenfw <ivan.petrochenko@gmail.com>
2018-11-02 16:53:53 +0300

    Props tests added for Carousel component

--- src/components/__tests__/Carousel.js
+++ src/components/__tests__/Carousel.js
@@ -8,15 +8,59 @@ import Carousel, { Value } from 'components/Carousel'
 
 configure({ adapter: new Adapter() })
 
-describe('<Carousel /> snapshot test', () => {
-  it('should match the snapshot', () => {
-    const tree = shallow(
-      <Carousel direction="up">
-        <Value>One</Value>
-      </Carousel>
-    )
+describe('<Carousel />', () => {
+  describe('snapshot test', () => {
+    it('should match the snapshot', () => {
+      const tree = shallow(
+        <Carousel direction="up">
+          <Value>One</Value>
+        </Carousel>
+      )
 
-    expect(toJson(tree)).toMatchSnapshot()
+      expect(toJson(tree)).toMatchSnapshot()
+    })
+  })
+
+  describe('props', () => {
+    it('sets CSSTransition\'s classNames prop to \'scroll-up\'', () => {
+      const wrapper = shallow(
+        <Carousel direction="up">
+          <Value>One</Value>
+        </Carousel>
+      )
+
+      expect(wrapper.find('CSSTransition').props().classNames).toBe('scroll-up')
+    })
+
+    it('sets CSSTransition\'s classNames prop to \'scroll-down\'', () => {
+      const wrapper = shallow(
+        <Carousel direction="down">
+          <Value>One</Value>
+        </Carousel>
+      )
+
+      expect(wrapper.find('CSSTransition').props().classNames).toBe('scroll-down')
+    })
+
+    it('sets CSSTransition\'s classNames prop to \'scroll-left\'', () => {
+      const wrapper = shallow(
+        <Carousel direction="left">
+          <Value>One</Value>
+        </Carousel>
+      )
+
+      expect(wrapper.find('CSSTransition').props().classNames).toBe('scroll-left')
+    })
+
+    it('sets CSSTransition\'s classNames prop to \'scroll-right\'', () => {
+      const wrapper = shallow(
+        <Carousel direction="right">
+          <Value>One</Value>
+        </Carousel>
+      )
+
+      expect(wrapper.find('CSSTransition').props().classNames).toBe('scroll-right')
+    })
   })
 })
 
