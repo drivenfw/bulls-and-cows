905049a12c1326edc9330d17531425b9db75b46b
drivenfw <ivan.petrochenko@gmail.com>
2018-11-04 21:15:14 +0300

    Checkbox component's snapshot tests updated

--- src/components/__tests__/Checkbox.js
+++ src/components/__tests__/Checkbox.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { configure, shallow } from 'enzyme'
+import { configure, mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
+import 'jest-styled-components'
 
 import Checkbox from 'components/Checkbox'
 
@@ -10,16 +11,45 @@ configure({ adapter: new Adapter() })
 
 describe('<Checkbox />', () => {
   describe('snapshot test', () => {
-    it('should match the snapshot', () => {
-      const tree = shallow(
-        <Checkbox 
-          checked 
-          label="Custom" 
-          onChange={() => {}}
-        />
-      )
+    describe('checked', () => {
+      it('should match the snapshot', () => {
+        const tree = mount(
+          <Checkbox 
+            checked 
+            label="Custom" 
+            onChange={() => {}}
+          />
+        )
 
-      expect(toJson(tree)).toMatchSnapshot()
+        expect(toJson(tree)).toMatchSnapshot()
+      })
+    })
+
+    describe('unchecked', () => {
+      it('should match the snapshot', () => {
+        const tree = mount(
+          <Checkbox 
+            checked={false}
+            label="Custom" 
+            onChange={() => {}}
+          />
+        )
+
+        expect(toJson(tree)).toMatchSnapshot()
+      })
+    })
+
+    describe('no label', () => {
+      it('should match the snapshot', () => {
+        const tree = mount(
+          <Checkbox 
+            checked={false}
+            onChange={() => {}}
+          />
+        )
+
+        expect(toJson(tree)).toMatchSnapshot()
+      })
     })
   })
 })
