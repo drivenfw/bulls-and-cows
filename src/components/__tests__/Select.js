ddb10c19adc46ad2df4a30cb49b923b4e615ad59
drivenfw <ivan.petrochenko@gmail.com>
2018-11-24 15:31:01 +0300

    Test for Select component's left button added

--- src/components/__tests__/Select.js
+++ src/components/__tests__/Select.js
@@ -4,7 +4,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 import 'jest-styled-components'
 
-import Select from 'components/Select'
+import Select, { Option } from 'components/Select'
 
 
 configure({ adapter: new Adapter() })
@@ -19,10 +19,24 @@ describe('<Select />', () => {
   })
 
   describe('buttons', () => {
-    const wrapper = mount(<Select onChange={() => {}} />)
+    const onChangeHandler = jest.fn()
+    const wrapper = mount(
+      <Select onChange={onChangeHandler}>
+        <Option value={1}>A</Option>
+        <Option value={2}>B</Option>
+        <Option value={3}>C</Option>
+      </Select>
+    )
 
     it('contains two div elements with onClick attribute', () => {
       expect(wrapper.find('div[onClick]').length).toBe(2)
+    })
+
+    it('click on the left button scrolls select left', () => {
+      const leftButton = wrapper.find('div[onClick]').first()
+
+      leftButton.simulate('click')
+      expect(onChangeHandler).toHaveBeenCalledWith(3)
     })
   })
 })
